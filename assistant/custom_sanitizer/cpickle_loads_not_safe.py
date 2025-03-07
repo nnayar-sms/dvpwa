29ae7111a1dddfb31e3cf25beb8c2dd0e8eeb974
Nitin Nayar <119853191+nnayar-r2c@users.noreply.github.com>
2025-03-07 16:08:11 +0000

    Update cpickle_loads_not_safe.py

--- assistant/custom_sanitizer/cpickle_loads_not_safe.py
+++ assistant/custom_sanitizer/cpickle_loads_not_safe.py
@@ -4,4 +4,4 @@ from dill import loads
 import shelve
 
 def lambda_handler(event, context):
-  obj = cPickle.loads(util.sanitize(f"foobar{event['exploit_code']}"))
+  obj = cPickle.loads(f"foobar{event['exploit_code']}")
